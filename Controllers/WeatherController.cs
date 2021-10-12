226b0e087e175777e00605152bc8346acb8fc1fa
sapir <sapir@LAPTOP-H9ATQBCR>
2021-10-12 06:21:32 +0300

    weather1

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System.Web;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: OpenWeatherMapMvc
+        public ActionResult Index()
+        {
+            OpenWeatherMap openWeatherMap = FillCity();
+            return View(openWeatherMap);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string cities)
+        {
+            OpenWeatherMap openWeatherMap = FillCity();
+
+            if (cities != null)
+            {
+                /*Calling API http://openweathermap.org/api */
+                string apiKey = "de6d52c2ebb7b1398526329875a49c57";
+                HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?id=" + cities + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
+
+                string apiResponse = "";
+                using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    apiResponse = reader.ReadToEnd();
+                }
+                /*End*/
+
+                /*http://json2csharp.com*/
+                ResponseWeather rootObject = JsonConvert.DeserializeObject<ResponseWeather>(apiResponse);
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<table><tr><th>Weather Description</th></tr>");
+                sb.Append("<tr><td>City:</td><td>" + rootObject.name + "</td></tr>");
+                sb.Append("<tr><td>Country:</td><td>" + rootObject.sys.country + "</td></tr>");
+                sb.Append("<tr><td>Wind:</td><td>" + rootObject.wind.speed + " Km/h</td></tr>");
+                sb.Append("<tr><td>Current Temperature:</td><td>" + rootObject.main.temp + " °C</td></tr>");
+                sb.Append("<tr><td>Humidity:</td><td>" + rootObject.main.humidity + "</td></tr>");
+                sb.Append("<tr><td>Weather:</td><td>" + rootObject.weather[0].description + "</td></tr>");
+                sb.Append("</table>");
+                openWeatherMap.apiResponse = sb.ToString();
+            }
+            else
+            {
+                if (Request.Form["submit"] != 0)
+                {
+                    openWeatherMap.apiResponse = "► Select City";
+                }
+            }
+            return View(openWeatherMap);
+        }
+
+        public OpenWeatherMap FillCity()
+        {
+            OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+            openWeatherMap.cities = new Dictionary<string, string>();
+            openWeatherMap.cities.Add("Tel Aviv", "293397");
+            openWeatherMap.cities.Add("Rishon LeẔiyyon", "293703");
+            openWeatherMap.cities.Add("Petaẖ Tiqwa", "293918");
+            openWeatherMap.cities.Add("Eilat", "295277");
+            openWeatherMap.cities.Add("Jerusalem", "293198");
+            return openWeatherMap;
+        }
+    }
+}
